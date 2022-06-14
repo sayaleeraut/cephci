36274c852a9ab8987cb848eec2b00e9e8ab826a6
anrao19 <83442624+anrao19@users.noreply.github.com>
2022-06-14 13:22:51 +0530

    Update upstream_suite_executor.groovy

--- pipeline/upstream_suite_executor.groovy
+++ pipeline/upstream_suite_executor.groovy
@@ -49,6 +49,7 @@ node(nodeName) {
             }
         }
         stage('Execute Testsuites') {
+            overrides.build = upstreamVersion
             def tags = "${buildType}, stage-1"
             print("Fetching stages")
             fetchStages = sharedLib.fetchStagesUpstream(tags, overrides, testResults, upstreamVersion)
