68eee510ef4cd5abe0871bfcb56795fdab963112
anrao19 <83442624+anrao19@users.noreply.github.com>
2022-06-15 15:34:51 +0530

    Update run.py

--- run.py
+++ run.py
@@ -449,6 +449,7 @@ def run(args):
     if build and build not in ["released"]:
         if build == "upstream":
             upstream_build = args["--upstream-build"]
+            print(upstream_build)
         base_url, docker_registry, docker_image, docker_tag = fetch_build_artifacts(
             build, rhbuild, platform, upstream_build
         )
